bd7556302f4fdee6bec2914a1cfcc2ef39889a3a
Bop-bili-bop <pelmen1109@gmail.com>
2025-04-04 17:12:21 +0300

    add_sign_in

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,41 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Metadata } from "next";
+import Link from "next/link";
+import Image from "next/image";
+import { APP_NAME } from "@/lib/constants";
+export const metadata: Metadata = {
+  title: "Sign In",
+};
+
+const signInPage = () => {
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <Card>
+        <CardHeader className="space-y-4">
+          <Link href="/" className="flex-center">
+            <Image
+              src="images/logo.svg"
+              width={100}
+              height={100}
+              alt={`${APP_NAME} logo`}
+              priority={true}
+            />
+          </Link>
+          <CardTitle className="flex-center">Sign In</CardTitle>
+          <CardDescription className="text-center">
+            Sign in to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">{/* FORM */}</CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default signInPage;
--- lib/validator.ts
+++ lib/validator.ts
@@ -2,11 +2,11 @@ import { z } from "zod";
 import { formatNumberWithDecimal } from "./utils";
 
 const currency = z
-    .string()
-    .refine(
+  .string()
+  .refine(
     (value) => /^\d+(\.\d{2})?/.test(formatNumberWithDecimal(Number(value))),
     "Price must have exactly two decimal places"
-)
+  );
 
 export const insertProductSchema = z.object({
   name: z.string().min(3, "Name must be at least 3 characters"),
@@ -19,4 +19,9 @@ export const insertProductSchema = z.object({
   isFeatured: z.boolean(),
   banner: z.string().nullable(),
   price: currency,
+});
+
+export const signInFormSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be atleast 6 characters"),
 });
