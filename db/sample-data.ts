659f8c79e4134555bee78acc802ce33f92bc1ac0
Bop-bili-bop <pelmen1109@gmail.com>
2025-04-04 16:30:32 +0300

    add_auth_ts

--- db/sample-data.ts
+++ db/sample-data.ts
@@ -1,4 +1,20 @@
+import {hashSync} from 'bcrypt-ts-edge'
+
 const sampleData = {
+  users: [
+    {
+      name: 'Nigger',
+      email: 'admin@exapmple.com',
+      password: hashSync('123', 10),
+      role: 'admin'
+    },
+    {
+      name: 'Sergiy',
+      email: 'user@exapmple.com',
+      password: hashSync('123', 10),
+      role: 'user'
+    },
+  ],
   products: [
     {
       name: 'Polo Sporting Stretch Shirt',
